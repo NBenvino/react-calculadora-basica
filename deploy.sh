34d167415c3f6bd533545bf37b45ff16ac3383aa
NBenvino <nicolas.benvino@gmail.com>
2022-10-10 14:14:29 -0300

    .

--- deploy.sh
+++ deploy.sh
@@ -25,6 +25,6 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
-git push -f git@github.com:nbenvino/calculadora-basica-react.git main:gh-pages
+git push -f git@github.com:nbenvino/react-calculadora-basica.git main:gh-pages
 
 cd -
